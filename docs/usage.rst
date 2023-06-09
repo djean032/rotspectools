71f193289389a251ec1025f83f6b5894250b75be
Dairen Jean <dairenjean@Dairens-MBP.home>
2023-06-09 18:34:56 -0400

    initial publication

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use Rotational Spectroscopy Tools in a project::
+
+    import rotspectools
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Dairen Jean",
+    author_email='djean032@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Package for automating data analysis and visualization.",
+    entry_points={
+        'console_scripts': [
+            'rotspectools=rotspectools.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='rotspectools',
+    name='rotspectools',
+    packages=find_packages(include=['rotspectools', 'rotspectools.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/djean032/rotspectools',
+    version='0.1.0',
+    zip_safe=False,
+)

1b82b2f9a624471c564bda4fa07d6929dfb6cfae
Dairen Jean <59145040+djean032@users.noreply.github.com>
2023-06-10 21:34:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/djean032/rotspectools',
-    version='0.1.0',
+    version='0.1.4',
     zip_safe=False,
 )

091905e13cd578d779d7a9526aa63ed62bd9bca4
Dairen Jean <dairenjean@Dairens-MacBook-Pro.local>
2023-07-24 11:45:02 -0500

    added new build

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/djean032/rotspectools',
-    version='0.1.4',
+    version='0.1.9',
     zip_safe=False,
 )
